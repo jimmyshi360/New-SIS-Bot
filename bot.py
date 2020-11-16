298d4b1fcb7030e548040e49395a8038bd48ad17
rsambas1@jhu.edu <jimmyshi360@gmail.com>
2020-11-16 01:48:15 -0500

    updated bot for microsoft login

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException  
 from selenium.webdriver.support.ui import Select 
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.action_chains import ActionChains
+
 import datetime
 import os 
 import sys
+
+#I FUCKING HATE WRITING NEW BOTS. FUCKKKKKKK
 
 usernameStr = 'REPLACE'
 passwordStr = 'REPLACE'
@@ -18,22 +24,57 @@ except BaseException:
 	print("\nError: This script should be run with the following (valid) flags:\n python bot.py SIS_Username SIS_Password\n")
 	sys.exit(-1)
 
-browser = webdriver.Chrome()
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
 browser.get(('https://sis.jhu.edu/sswf/'))
 nextButton = browser.find_element_by_id('linkSignIn')
 nextButton.click()
+WebDriverWait(browser, 10)
 
-username = browser.find_element_by_id('USERdisplay')
+username = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.NAME, 'loginfmt')))
 username.send_keys(usernameStr)
 
 WebDriverWait(browser, 10)
-password = browser.find_element_by_id('PASSWORD')
-password.send_keys(passwordStr)
 
-submit1button = browser.find_element_by_id("submit1")
+submit1button = browser.find_element_by_id("idSIButton9")
 submit1button.click()
 
+
+WebDriverWait(browser, 10)
+password = browser.find_element_by_name('passwd')
+password.send_keys(passwordStr)
+
+WebDriverWait(browser, 10)
+
+#ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+#submit2button = WebDriverWait(browser, 20,ignored_exceptions=ignored_exceptions).until(EC.presence_of_element_located((By.ID, "idSIButton9")))
+#submit2button.click()
+'''
+def find(driver):
+    element = browser.find_elements_by_class_name("button ext-button primary ext-primary")[0]
+    if element:
+        return element
+    else:
+        return False
+submit2button = WebDriverWait(browser, 20).until(find)
+submit2button.click()
+'''
+
+staleElement = True; 
+
+while staleElement:
+    try:
+        submit = WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.ID, 'idSIButton9')))
+        submit.click()
+        staleElement = False
+
+    except StaleElementReferenceException:
+
+        staleElement = True
+
+
 WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "aspnetForm")))
+
 browser.get("https://sis.jhu.edu/sswf/SSS/EnrollmentCart/SSS_EnrollmentCart.aspx?MyIndex=88199")
 
 
